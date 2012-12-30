46a8b72fad86b117ccbaafeb28d37101efee96a6
fabregas <kksstt@gmail.com>
2012-12-30 11:24:48 +0200

    base perf tests implemented

---
+++ tests/perf/base_perf_test.py
@@ -0,0 +1,136 @@
+from primitives import *
+
+def to_str(val):
+    if type(val) == timedelta:
+        return str(val.total_seconds())
+    return str(val)
+
+def check_errors(errors_queue):
+    time.sleep(1)
+    err_msg = ''
+    while True:
+        try:
+            err_msg += '%s\n'%errors_queue.get(False)
+        except Empty:
+            break
+
+    if err_msg:
+        raise Exception(err_msg)
+
+def collect_perf_stat(nodes_list):
+    stat = collect_nodes_stat(nodes_list)
+    put_data_block_time = put_client_time = get_keys_info_time = get_data_block_time = timedelta()
+
+    for node_stat in stat.values():
+        put_data_block_time += to_dt(node_stat['methods_stat']['PutDataBlock']['avg_proc_time'])
+        get_data_block_time += to_dt(node_stat['methods_stat']['GetDataBlock']['avg_proc_time'])
+        put_client_time += to_dt(node_stat['methods_stat']['ClientPutData']['avg_proc_time'])
+        get_keys_info_time += to_dt(node_stat['methods_stat']['GetKeysInfo']['avg_proc_time'])
+
+    return {'put_data_block_time': put_data_block_time/len(stat), \
+                'put_client_time': put_client_time/len(stat), \
+                'get_keys_info_time': get_keys_info_time/len(stat), \
+                'get_data_block_time': get_data_block_time/len(stat)}
+
+
+
+def no_parallel_scenario(nodes_list, block_size, blocks_count):
+    print '=> sequential scenario with blocksize=%s and blocks_count=%s ...'%(block_size, blocks_count)
+    memmon = MemoryMonitor()
+
+    keys_queue = Queue()
+    errors_queue = Queue()
+
+    memmon.start(nodes_list)
+    try:
+        put_dt = put_data(nodes_list, keys_queue, errors_queue, block_size, blocks_count)
+        check_errors(errors_queue)
+        get_dt = get_data(nodes_list, keys_queue, errors_queue)
+        check_errors(errors_queue)
+        stat = collect_perf_stat(nodes_list)
+    finally:
+        ret = memmon.stop()
+        min_mem, max_mem = ret
+
+    return put_dt, get_dt, stat['put_data_block_time'], stat['get_data_block_time'], stat['put_client_time'], stat['get_keys_info_time'], min_mem, max_mem
+
+
+def parallel_scenario(threads_count, nodes_list, block_size, blocks_count):
+    print '=> parallel scenario with %s threads (blocksize=%s, blocks_count=%s) ...'%(threads_count, block_size, blocks_count)
+    memmon = MemoryMonitor()
+
+    keys_queue = Queue()
+    errors_queue = Queue()
+
+    memmon.start(nodes_list)
+    try:
+        put_dt, th_avg_put_dt = parallel_put_data(threads_count, nodes_list, keys_queue, errors_queue, block_size, blocks_count)
+        check_errors(errors_queue)
+        get_dt, th_avg_get_dt = parallel_get_data(threads_count, nodes_list, keys_queue, errors_queue)
+        check_errors(errors_queue)
+        stat = collect_perf_stat(nodes_list)
+    finally:
+        min_mem, max_mem = memmon.stop()
+
+    return th_avg_put_dt, th_avg_get_dt, stat['put_data_block_time'], stat['get_data_block_time'], stat['put_client_time'], stat['get_keys_info_time'], min_mem, max_mem
+
+
+def test_scenario(nodes_list):
+    stat_data = []
+    MB = 1024*1024
+
+    stat_data.append(['nopar_1mb_1000']+ list(no_parallel_scenario(nodes_list, 1*MB, 1000)))
+    stat_data.append(['par_2_1mb_1000']+ list(parallel_scenario(2, nodes_list, 1*MB, 1000)))
+    stat_data.append(['par_4_1mb_1000']+ list(parallel_scenario(4, nodes_list, 1*MB, 1000)))
+
+    stat_data.append(['nopar_10mb_100']+ list(no_parallel_scenario(nodes_list, 10*MB, 100)))
+    stat_data.append(['par_2_10mb_100']+ list(parallel_scenario(2, nodes_list, 10*MB, 100)))
+    stat_data.append(['par_4_10mb_100']+ list(parallel_scenario(4, nodes_list, 10*MB, 100)))
+
+    stat_data.append(['nopar_100mb_10']+ list(no_parallel_scenario(nodes_list, 100*MB, 10)))
+    stat_data.append(['par_2_100mb_10']+ list(parallel_scenario(2, nodes_list, 100*MB, 10)))
+    stat_data.append(['par_4_100mb_10']+ list(parallel_scenario(4, nodes_list, 100*MB, 10)))
+
+    return stat_data
+
+def more_parallel_test(nodes_list):
+    stat_data = []
+    MB = 1024*1024
+
+    stat_data.append(['nopar_1mb_1000']+ list(no_parallel_scenario(nodes_list, 1*MB, 1000)))
+    stat_data.append(['par_2_1mb_1000']+ list(parallel_scenario(2, nodes_list, 1*MB, 1000)))
+    stat_data.append(['par_4_1mb_1000']+ list(parallel_scenario(4, nodes_list, 1*MB, 1000)))
+    stat_data.append(['par_8_1mb_1000']+ list(parallel_scenario(8, nodes_list, 1*MB, 1000)))
+    stat_data.append(['par_16_1mb_1000']+ list(parallel_scenario(16, nodes_list, 1*MB, 1000)))
+    stat_data.append(['par_32_1mb_1000']+ list(parallel_scenario(32, nodes_list, 1*MB, 1000)))
+
+    return stat_data
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print 'usage: %s <nodeaddr1>[,<nodeaddr2>...] <path to output file>'%sys.argv[0]
+        sys.exit(1)
+    t0 = datetime.now()
+    fout = None
+    TEST = more_parallel_test
+    try:
+        nodes_list = [n.strip() for n in sys.argv[1].split(',')]
+        fout = open(sys.argv[2], 'w')
+
+        stat_data = TEST(nodes_list)
+
+        fout.write('test_name, put_overal_time, get_overal_time, put_data_block_time, get_data_block_time, put_client_time, get_keys_info_time, fabnet_node_mem_min, fabnet_node_mem_max\n')
+        for item in stat_data:
+            fout.write(', '.join([to_str(i) for i in item])+'\n')
+
+        print 'Done!'
+        print 'Results saved to %s'%sys.argv[2]
+    except Exception, err:
+        print 'ERROR! %s'%err
+        sys.exit(1)
+    finally:
+        if fout:
+            fout.close()
+        print 'Processing time: %s'%(datetime.now()-t0)
+
---
+++ tests/perf/primitives.py
@@ -0,0 +1,216 @@
+#!/usr/bin/python
+import time
+from datetime import datetime, timedelta
+import json
+import os
+import sys
+import random
+
+from multiprocessing import Process, Queue, Event
+from Queue import Empty
+from fabnet.utils.logger import logger
+from nimbus_client.core.fabnet_gateway import FabnetGateway
+
+from fabnet.core.fri_server import FabnetPacketRequest
+from fabnet.core.fri_client import FriClient
+#logger.setLevel(logging.DEBUG)
+
+from Crypto import Random
+Random.atfork()
+
+class SecurityManagerMock:
+    def get_user_id(self):
+        return 'this is test USER ID string'
+
+    def get_client_cert(self):
+        return 'fake cert'
+
+    def get_client_cert_key(self):
+        return
+
+    def encrypt(self, data):
+        return data
+
+    def decrypt(self, data):
+        return data
+
+def to_dt(dt_str):
+    parts = dt_str.split(':')
+    secs = 0
+    if len(parts) > 1:
+        secs = int(parts[-2])*60
+
+    return timedelta(0, secs+float(parts[-1]))
+
+
+
+def put_data(nodes_list, keys_queue, errors_queue, data_block_size, count=1, ret_queue=None):
+    Random.atfork()
+    data_block = Random.new().read(data_block_size)
+    dt = timedelta()
+    for i in xrange(count):
+        try:
+            node_addr = random.choice(nodes_list)
+            fabnet_gw = FabnetGateway(node_addr, SecurityManagerMock())
+            t0 = datetime.now()
+
+            primary_key, source_checksum = fabnet_gw.put(data_block, replica_count=2, wait_writes_count=3)
+            dt += (datetime.now() - t0)
+
+            keys_queue.put(primary_key)
+        except Exception, err:
+            errors_queue.put('[ERROR] PUT data block failed! %s'%err)
+
+    if ret_queue:
+        ret_queue.put(dt)
+    return dt
+
+
+def get_data(nodes_list, keys_queue, errors_queue, ret_queue=None):
+    dt = timedelta()
+    while True:
+        try:
+            primary_key = keys_queue.get(False)
+            node_addr = random.choice(nodes_list)
+            fabnet_gw = FabnetGateway(node_addr, SecurityManagerMock())
+            t0 = datetime.now()
+            data = fabnet_gw.get(primary_key, replica_count=2)
+            if not data:
+                raise Exception('no data found')
+            dt += (datetime.now() - t0)
+        except Empty:
+            break
+        except Exception, err:
+            errors_queue.put('[ERROR] GET data block failed! %s'%err)
+    if ret_queue:
+        ret_queue.put(dt)
+    return dt
+
+
+def collect_topology(node_ip):
+    print 'Collecting topology on %s'%node_ip
+    fri_client = FriClient()
+    packet_obj = FabnetPacketRequest(method='TopologyCognition')
+    ret_code, ret_msg = fri_client.call(node_ip, packet_obj)
+
+    if ret_code != 0:
+        print '[ERROR] TopologyCognition operation does not started: %s'%ret_msg
+
+    time.sleep(3)
+
+def collect_nodes_stat(nodes_list):
+    collected_stat = {}
+    fri_client = FriClient()
+    for node_ip in nodes_list:
+        #print 'Collecting statistic from %s'%node_ip
+        packet_obj = FabnetPacketRequest(method='NodeStatistic', sync=True)
+        ret_packet = fri_client.call_sync(node_ip, packet_obj)
+        if ret_packet.ret_code != 0:
+            raise Exception('[ERROR] no statistic: %s'%ret_packet.ret_message)
+
+        collected_stat[node_ip] = ret_packet.ret_parameters
+
+    return collected_stat
+
+
+def parallel_put_data(threads_count, nodes_list, keys_queue, errors_queue, data_block_size, count):
+    threads = []
+    resp_queue = Queue()
+    for i in xrange(threads_count):
+        p_count = count/threads_count
+        if i == threads_count-1:
+            p_count += count % threads_count
+        p = Process(target=put_data, args=(nodes_list, keys_queue, errors_queue, data_block_size, p_count, resp_queue))
+        threads.append(p)
+
+    time.sleep(1)
+    t0 = datetime.now()
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+    dt = datetime.now() - t0
+
+    dt_list = []
+    while True:
+        try:
+            c_dt = resp_queue.get(False)
+            dt_list.append(c_dt)
+        except Empty:
+            break
+
+    return dt, sum(dt_list, timedelta())/len(dt_list)
+
+
+def parallel_get_data(threads_count, nodes_list, keys_queue, errors_queue):
+    threads = []
+    resp_queue = Queue()
+    for i in xrange(threads_count):
+        p_count = keys_queue.qsize() / threads_count
+        if i == threads_count-1:
+            p_count += keys_queue.qsize() % threads_count
+        queue = Queue()
+        for j in xrange(p_count):
+            queue.put(keys_queue.get())
+
+        p = Process(target=get_data, args=(nodes_list, queue, errors_queue, resp_queue))
+        threads.append(p)
+
+    time.sleep(1)
+    t0 = datetime.now()
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+    dt = datetime.now() - t0
+
+    dt_list = []
+    while True:
+        try:
+            c_dt = resp_queue.get(False)
+            dt_list.append(c_dt)
+        except Empty:
+            break
+
+    return dt, sum(dt_list, timedelta())/len(dt_list)
+
+
+class MemoryMonitor:
+    def __init__(self):
+        self.proc = None
+        self.stop_flag = Event()
+        self.ret_queue = Queue()
+
+    def monitor_memory(self, nodes_list, stop_flag, ret_queue):
+        min_memory = 10000000
+        max_memory = 0
+        while not stop_flag.is_set():
+            time.sleep(1)
+            try:
+                stat = collect_nodes_stat(nodes_list)
+            except Exception, err:
+                print 'monitor_memory error: %s'%err
+                continue
+
+            for node_stat in stat.values():
+                mem = int(node_stat['memory'])
+                if mem < min_memory:
+                    min_memory = mem
+                if mem > max_memory:
+                    max_memory = mem
+
+        ret_queue.put((min_memory, max_memory))
+
+    def start(self, nodes_list):
+        self.proc = Process(target=self.monitor_memory, args=(nodes_list, self.stop_flag, self.ret_queue))
+        self.proc.start()
+
+    def stop(self):
+        if self.proc:
+            self.stop_flag.set()
+            self.proc.join()
+            return self.ret_queue.get()
+
+
